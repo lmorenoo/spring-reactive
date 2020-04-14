6f3f27f62c204f255d64efa832cb5dc1a8648137
alejandropalacioduque <epalaciod@psl.com.co>
2020-04-13 20:03:07 -0500

    Implemented matricula Controller

--- src/main/java/com/mitocode/springreactive/controller/MatriculaController.java
+++ src/main/java/com/mitocode/springreactive/controller/MatriculaController.java
@@ -1,5 +1,6 @@
 package com.mitocode.springreactive.controller;
 
+import com.mitocode.springreactive.document.Estudiante;
 import com.mitocode.springreactive.document.Matricula;
 import com.mitocode.springreactive.dto.MatriculaDTO;
 import com.mitocode.springreactive.exceptions.MatriculaException;
@@ -8,6 +9,7 @@ import com.mitocode.springreactive.service.IEstudianteService;
 import com.mitocode.springreactive.service.IMatriculaService;
 import com.mitocode.springreactive.utils.mapper.MatriculaMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -44,10 +47,28 @@ public class MatriculaController {
     @PostMapping
     public Mono<ResponseEntity<MatriculaDTO>> registrar(@Valid @RequestBody MatriculaDTO matricula,
                                                         final ServerHttpRequest req){
-        // TODO implementar creacion de matricula
 
-        Matricula matriculaEntity = new Matricula();
-        return matriculaService.registrar(matriculaEntity)
+//        Mono<Matricula> matriculaMono = Mono.just(matricula).flatMap(m ->
+//                matriculaService.listarPorEstudianteId(m.getEstudianteId())
+//                        .switchIfEmpty(estudianteService.listarPorId(m.getEstudianteId())
+//                                .zipWith(cursoService.listarPorIds(m.getCursos()).collectList())
+//                                .flatMap(tuple ->
+//                                        matriculaService.registrar(new Matricula(tuple.getT1(), tuple.getT2()))
+//                                )
+//                        )
+//        );
+
+        Mono<Matricula> matriculaMono = Mono.just(matricula).flatMap(m ->
+                matriculaService.listarPorEstudianteId(m.getEstudianteId())
+                        .switchIfEmpty(estudianteService.listarPorId(m.getEstudianteId())
+                                .zipWith(cursoService.listarPorIds(m.getCursos()).collectList())
+                                .flatMap(tuple ->
+                                        matriculaService.registrar(new Matricula(tuple.getT1(), tuple.getT2()))
+                                )
+                        )
+        );
+
+        return matriculaMono
                 .map(p -> ResponseEntity.created(URI.create(req.getURI().toString().concat("/")))
                         .contentType(MediaType.APPLICATION_STREAM_JSON)
                         .body(matriculaMapper.toDTO(p))
--- src/main/java/com/mitocode/springreactive/utils/mapper/MatriculaMapper.java
+++ src/main/java/com/mitocode/springreactive/utils/mapper/MatriculaMapper.java
@@ -2,15 +2,17 @@ package com.mitocode.springreactive.utils.mapper;
 
 import com.mitocode.springreactive.document.Matricula;
 import com.mitocode.springreactive.dto.MatriculaDTO;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class MatriculaMapper {
 
     public MatriculaDTO toDTO(Matricula matricula) {
         MatriculaDTO matriculaDTO = new MatriculaDTO();
-        BeanUtils.copyProperties(matricula, matriculaDTO);
+        matriculaDTO.setEstudianteId(matricula.getEstudiante().getId());
+        matriculaDTO.setCursos(matricula.getCursos().stream().map(curso -> curso.getId()).collect(Collectors.toList()));
         return matriculaDTO;
     }
 
